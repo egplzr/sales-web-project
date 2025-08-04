a051ae9f8dfa4f7814d3728a4dc6c91311a806db
egplzr <egpelizaro@gmail.com>
2025-08-04 12:35:13 -0300

    feat: add validation to seller create and edit views

--- SalesMvcProject/Controllers/SellersController.cs
+++ SalesMvcProject/Controllers/SellersController.cs
@@ -28,6 +28,13 @@ public class SellersController(SellerService sellerService, DepartmentService de
     [ValidateAntiForgeryToken]
     public IActionResult Create(Seller seller)
     {
+        if (!ModelState.IsValid)
+        {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel{Seller = seller, Departments = departments};
+            return View(viewModel);
+        }
+        
         _sellerService.Insert(seller);
         return RedirectToAction(nameof(Index));
     }
@@ -77,6 +84,12 @@ public class SellersController(SellerService sellerService, DepartmentService de
     [ValidateAntiForgeryToken]
     public IActionResult Edit(int id, Seller seller)
     {
+        if (!ModelState.IsValid)
+        {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel{Seller = seller, Departments = departments};
+            return View(viewModel);
+        }
         if (id != seller.Id) return RedirectToAction(nameof(Error), new{message = "Id mismatch"});;
 
         try
